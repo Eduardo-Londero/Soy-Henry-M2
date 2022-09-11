1933d43e1ecf8186b147e7463b791441af024ed6
Eduardo-Londero <edulondero2407@gmail.com>
2022-09-11 11:55:40 -0300

    subiendo primer parte de la tarea del m2

--- FT-M2-master/01-DOM/homework/DOMhomework.js
+++ FT-M2-master/01-DOM/homework/DOMhomework.js
@@ -1,12 +1,15 @@
 // Crear un array vacío llamado 'toDoItems'
 // Tu codigo acá:
 
+var toDoItems = [];
 
 // En la página 'index.html' hay un elemento span cuyo texto es 'Aplicación creada por:'.
 // Usando querySelector seleccionar dicho span por su id ('createdBy') y luego usando innerHTML
 // agregar tu nombre al final del texto actual. Ej: 'Aplicación creada por Franco'
 // Tu código acá:
 
+var span = document.querySelector('#createdBy');
+span.innerHTML = span.innerHTML + ' Eduardo';
 
 
 // Crear una clase denominada 'ToDo' cuyo constructor debe recibir un único parámetro del tipo string
